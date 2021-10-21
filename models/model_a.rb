e6935172d1a7f6fdd297672e63d02272606b8b0f
Becky Nisttahuz <benistta@hotmail.com>
2021-10-21 14:27:40 -0600

    Add Airport to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+airport = {
+  "name" => 'Denver International Airport',
+  "location" => 'Denver, CO',
+  "baggage_claim_carousels" => 19,
+  "levels" => 6,
+  "international_airport" => true,
+  "small_airport" => false,
+  "important_airlines" => ['american', 'delta', 'southwest'],
+  "accessibility" => ['elevators','buses','taxis']
+}
