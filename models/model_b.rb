35196a3d62f461e1707dc9897acdd102682b2d4b
Becky Nisttahuz <benistta@hotmail.com>
2021-10-21 14:28:36 -0600

    Add vehicle to model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+vehicle = {
+  "name" => 'Honda Civic',
+  "category" => 'compact',
+  "year" => 2022,
+  "rating" => 9.5,
+  "good_appearance" => true,
+  "fast" => false,
+  "highs" => ['upscale', 'comfortable'],
+  "lows" => ['expensive', 'small']
+}
