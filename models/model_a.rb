f62668a73b1599df4d4e7797fee6eab24bbb29b1
benistta <92293363+benistta@users.noreply.github.com>
2021-10-25 10:22:13 -0600

    Update model_a.rb

--- models/model_a.rb
+++ models/model_a.rb
@@ -3,8 +3,8 @@ airport = {
   "location" => 'Denver, CO',
   "baggage_claim_carousels" => 19,
   "levels" => 6,
-  "international_airport" => true,
-  "small_airport" => false,
+  "is_international_airport" => true,
+  "is_small_airport" => false,
   "important_airlines" => ['american', 'delta', 'southwest'],
-  "accessibility" => ['elevators','buses','taxis']
+  "accessibility_features" => ['elevators','buses','taxis']
 }
